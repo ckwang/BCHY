eac999a0ec68800442828ec3b61d29c59eb36276
Bunkie <Bunkie@18.111.124.169>
2011-01-05 11:47:27 -0500

    Initial Recyclers build an antenna on the initial Constructor as well.

--- teams/team017/AI/BuildingAI.java
+++ teams/team017/AI/BuildingAI.java
@@ -46,9 +46,16 @@ public class BuildingAI extends AI {
 	private void init() {
 		try {
 			// install an antenna to the adjacent recycler
-			MapLocation recyclerLocation = senseAdjacentRecycler();
-			if ( recyclerLocation != null && myRC.getTeamResources() >= 2 * ComponentType.ANTENNA.cost ) {
-				builder.build(ComponentType.ANTENNA, recyclerLocation, RobotLevel.ON_GROUND);
+			RobotInfo info = senseAdjacentChassis(Chassis.LIGHT);
+			if ( info != null && myRC.getTeamResources() >= 2 * ComponentType.ANTENNA.cost 
+					&& !containsComponent(info.components, ComponentType.ANTENNA) ) {
+				builder.build(ComponentType.ANTENNA, info.location, RobotLevel.ON_GROUND);
+			}
+			myRC.yield();
+			
+			info = senseAdjacentChassis(Chassis.BUILDING);
+			if ( info != null && myRC.getTeamResources() >= 2 * ComponentType.ANTENNA.cost ) {
+				builder.build(ComponentType.ANTENNA, info.location, RobotLevel.ON_GROUND);
 			}
 		} catch (Exception e) {
 			System.out.println("caught exception:");
@@ -57,20 +64,28 @@ public class BuildingAI extends AI {
 	}
 	
 	/***
-	 * Sense nearby robots and return the location of one Recycler if there exists one
+	 * Sense nearby robots and return the location of one robot with specific chassis.
+	 * Return none if there is no such robot.
 	 * @throws GameActionException
 	 */
-	private MapLocation senseAdjacentRecycler() throws GameActionException {
+	private RobotInfo senseAdjacentChassis(Chassis chassis) throws GameActionException {
 		Robot[] robots = sensor.senseNearbyGameObjects(Robot.class);
 		for ( Robot r : robots ) {
 			if (r.getTeam() == myRC.getTeam()) {
 				RobotInfo info = sensor.senseRobotInfo(r);
-				if (info.chassis == Chassis.BUILDING)
-					return info.location;
+				if (info.chassis == chassis)
+					return info;
 			}
 		}
 		
 		return null;
+	}
+	
+	private boolean containsComponent(ComponentType[] list, ComponentType com) {
+		for ( ComponentType c : list ) {
+			if (c == com)	return true;
+		}
+		return false;
 	}
 
 }
