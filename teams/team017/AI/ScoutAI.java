d4776351be80f5d6c2f9462ebdffb15c3dcf62db
bunkie <bunkie@dhcp-18-111-119-139.dyn.mit.edu>
2011-01-18 23:15:50 -0500

    added ScoutAI template

---
+++ teams/team017/AI/ScoutAI.java
@@ -0,0 +1,23 @@
+package team017.AI;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class ScoutAI extends AI {
+
+	public ScoutAI(RobotController rc) {
+		super(rc);
+	}
+
+	@Override
+	public void proceed() {
+		
+	}
+	
+	
+
+	@Override
+	protected void processMessages() throws GameActionException {
+	}
+
+}
