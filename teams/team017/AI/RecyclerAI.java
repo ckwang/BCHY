0df3a124deb5126b25ed2ccf4d8c8eb0929f3e57
yuchikuo <yuchikuo@18.111.100.108>
2011-01-23 16:21:43 -0500

    Que some WAR_MINER

--- teams/team017/AI/RecyclerAI.java
+++ teams/team017/AI/RecyclerAI.java
@@ -138,9 +138,9 @@ public class RecyclerAI extends BuildingAI {
 			constructingQueue.add(UnitType.FLYING_CONSTRUCTOR);
 			constructingQueue.add(UnitType.TELESCOPER);
 			constructingQueue.add(UnitType.FLYING_CONSTRUCTOR);
-			constructingQueue.add(UnitType.APOCALYPSE);
-			constructingQueue.add(UnitType.APOCALYPSE);
-			constructingQueue.add(UnitType.APOCALYPSE);
+			constructingQueue.add(UnitType.CHRONO_APOCALYPSE);
+			constructingQueue.add(UnitType.CHRONO_APOCALYPSE);
+			constructingQueue.add(UnitType.CHRONO_APOCALYPSE);
 			constructingQueue.add(UnitType.WAR_MINER);
 			constructingQueue.add(UnitType.WAR_MINER);
 			constructingQueue.add(UnitType.WAR_MINER);
